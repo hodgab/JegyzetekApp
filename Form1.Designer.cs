ceb997d30bd6eaf7ad5207507195830d55bb14f6
hodgab <gabor.hodovan14@gmail.com>
2023-03-15 17:13:26 +0100

    jegyzetek

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace JegyzetekApp
+{
+    partial class form_foablak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_foablak));
+            this.pnl_fejlec = new System.Windows.Forms.Panel();
+            this.btn_hozzaad = new System.Windows.Forms.Button();
+            this.txt_uj_jegyzet = new System.Windows.Forms.TextBox();
+            this.lbl_cim = new System.Windows.Forms.Label();
+            this.pnl_fo = new System.Windows.Forms.Panel();
+            this.btn_bezaras = new System.Windows.Forms.Button();
+            this.pnl_fejlec.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_fejlec
+            // 
+            this.pnl_fejlec.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(145)))), ((int)(((byte)(126)))));
+            this.pnl_fejlec.Controls.Add(this.btn_bezaras);
+            this.pnl_fejlec.Controls.Add(this.btn_hozzaad);
+            this.pnl_fejlec.Controls.Add(this.txt_uj_jegyzet);
+            this.pnl_fejlec.Controls.Add(this.lbl_cim);
+            this.pnl_fejlec.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_fejlec.Location = new System.Drawing.Point(0, 0);
+            this.pnl_fejlec.Name = "pnl_fejlec";
+            this.pnl_fejlec.Size = new System.Drawing.Size(485, 90);
+            this.pnl_fejlec.TabIndex = 0;
+            this.pnl_fejlec.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_fejlec_Paint);
+            // 
+            // btn_hozzaad
+            // 
+            this.btn_hozzaad.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_hozzaad.BackgroundImage")));
+            this.btn_hozzaad.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_hozzaad.FlatAppearance.BorderSize = 0;
+            this.btn_hozzaad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_hozzaad.Location = new System.Drawing.Point(391, 42);
+            this.btn_hozzaad.Name = "btn_hozzaad";
+            this.btn_hozzaad.Size = new System.Drawing.Size(33, 33);
+            this.btn_hozzaad.TabIndex = 3;
+            this.btn_hozzaad.UseVisualStyleBackColor = true;
+            this.btn_hozzaad.Click += new System.EventHandler(this.btn_hozzaad_Click);
+            // 
+            // txt_uj_jegyzet
+            // 
+            this.txt_uj_jegyzet.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_uj_jegyzet.Location = new System.Drawing.Point(140, 42);
+            this.txt_uj_jegyzet.Name = "txt_uj_jegyzet";
+            this.txt_uj_jegyzet.Size = new System.Drawing.Size(245, 33);
+            this.txt_uj_jegyzet.TabIndex = 2;
+            // 
+            // lbl_cim
+            // 
+            this.lbl_cim.AutoSize = true;
+            this.lbl_cim.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_cim.ForeColor = System.Drawing.Color.Black;
+            this.lbl_cim.Location = new System.Drawing.Point(3, 42);
+            this.lbl_cim.Name = "lbl_cim";
+            this.lbl_cim.Size = new System.Drawing.Size(138, 28);
+            this.lbl_cim.TabIndex = 1;
+            this.lbl_cim.Text = "Jegyzetek:";
+            // 
+            // pnl_fo
+            // 
+            this.pnl_fo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_fo.Location = new System.Drawing.Point(0, 90);
+            this.pnl_fo.Name = "pnl_fo";
+            this.pnl_fo.Size = new System.Drawing.Size(485, 360);
+            this.pnl_fo.TabIndex = 1;
+            // 
+            // btn_bezaras
+            // 
+            this.btn_bezaras.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_bezaras.BackgroundImage")));
+            this.btn_bezaras.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_bezaras.FlatAppearance.BorderSize = 0;
+            this.btn_bezaras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_bezaras.Location = new System.Drawing.Point(448, 12);
+            this.btn_bezaras.Name = "btn_bezaras";
+            this.btn_bezaras.Size = new System.Drawing.Size(25, 25);
+            this.btn_bezaras.TabIndex = 4;
+            this.btn_bezaras.UseVisualStyleBackColor = true;
+            this.btn_bezaras.Click += new System.EventHandler(this.btn_bezaras_Click);
+            // 
+            // form_foablak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(60)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.pnl_fo);
+            this.Controls.Add(this.pnl_fejlec);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "form_foablak";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.pnl_fejlec.ResumeLayout(false);
+            this.pnl_fejlec.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_fejlec;
+        private System.Windows.Forms.Label lbl_cim;
+        private System.Windows.Forms.Button btn_hozzaad;
+        private System.Windows.Forms.TextBox txt_uj_jegyzet;
+        private System.Windows.Forms.Panel pnl_fo;
+        private System.Windows.Forms.Button btn_bezaras;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JegyzetekApp
+{
+    public partial class form_foablak : Form
+    {
+        public form_foablak()
+        {
+            InitializeComponent();
+        }
+
+        private int jegy_szam = 0;
+
+        private void hozzaad(String jegyzet)
+        {
+            
+            
+            Panel panel = new Panel();
+            panel.Name = "pnl_" + jegy_szam;
+            panel.Dock= DockStyle.Top; 
+            panel.Height = 60;
+            panel.Location = new Point(0, jegy_szam * 60);
+
+            FontFamily family = new FontFamily("Century Gothic");
+            Label lbl_jegyzet = new Label();
+            lbl_jegyzet.Text = jegyzet;
+            lbl_jegyzet.Location = new Point(25, 19);
+            Font font = new Font(family, 15, FontStyle.Bold);
+            lbl_jegyzet.Font = font;
+            lbl_jegyzet.AutoSize = true;
+            lbl_jegyzet.ForeColor = System.Drawing.Color.FromArgb(235, 156, 92);
+
+            Button btn_torles = new Button();
+            btn_torles.Name = "btn_torles_" + jegy_szam;
+            btn_torles.Size = new Size(25, 25);
+            btn_torles.FlatStyle = FlatStyle.Flat;
+            Bitmap bitmap = new Bitmap("C:\\Users\\hp\\Desktop\\csharp\\JegyzetekApp\\JegyzetekApp\\ikonok\\kuka_ikon.png");
+            btn_torles.Image = bitmap;
+            btn_torles.Text = "";
+            btn_torles.Location = new Point(382, 19);
+            btn_torles.FlatAppearance.BorderSize = 0;
+            btn_torles.ImageAlign = ContentAlignment.MiddleCenter;
+            btn_torles.Click += new EventHandler(btn_torles_Click);
+            
+
+            Button btn_pipa = new Button();
+            btn_pipa.Name = "btn_pipa_" + jegy_szam;
+            btn_pipa.Size = new Size(25, 25);
+            btn_pipa.FlatStyle = FlatStyle.Flat;
+            Bitmap b = new Bitmap("C:\\Users\\hp\\Desktop\\csharp\\JegyzetekApp\\JegyzetekApp\\ikonok\\pipa_ikon.png");
+            btn_pipa.Image = b;
+            btn_pipa.Text = "";
+            btn_pipa.Location = new Point(425, 21);
+            btn_pipa.FlatAppearance.BorderSize = 0;
+            btn_pipa.ImageAlign = ContentAlignment.MiddleCenter;
+            btn_pipa.Click += new EventHandler(btn_pipa_Click);
+
+
+
+            panel.Controls.Add(lbl_jegyzet);
+            panel.Controls.Add(btn_torles);
+            panel.Controls.Add(btn_pipa);
+            panel.Show();
+            pnl_fo.Controls.Add(panel);
+            jegy_szam+= 1;
+        }
+
+        private void pnl_fejlec_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_hozzaad_Click(object sender, EventArgs e)
+        {
+            if(jegy_szam<=5)
+            {
+                String jegyzet = txt_uj_jegyzet.Text;
+                hozzaad(jegyzet);
+            }
+            else
+            {
+                MessageBox.Show("Először csináld meg a meglévőket");
+            }
+
+            txt_uj_jegyzet.Clear();
+            txt_uj_jegyzet.Focus();
+
+        }
+
+        private void btn_torles_Click(object sender, EventArgs e)
+        {
+            
+            Button kattint = (Button)sender;
+            String keresendo = kattint.Name;
+ 
+            string nev = "pnl_"+ Convert.ToString(keresendo[keresendo.Length - 1]);
+            foreach(Control c in pnl_fo.Controls)
+            {
+                if(c.Name == nev)
+                {
+                    pnl_fo.Controls.Remove(c);
+                    jegy_szam-= 1;
+                }
+            }
+
+        }
+
+        private void btn_pipa_Click(object sender, EventArgs e)
+        {
+            Button kattint = (Button)sender;
+            String keresendo = kattint.Name;
+
+            String nev = "pnl_" + Convert.ToString(keresendo[keresendo.Length - 1]);
+            
+            foreach (Control c in pnl_fo.Controls)
+            {
+                if (c.Name == nev)
+                {
+                    c.BackColor = Color.Green;
+                    c.Controls.Clear();
+                    
+                    Thread.Sleep(500);
+                    pnl_fo.Controls.Remove(c);
+                    jegy_szam -= 1;
+                       
+
+                }
+            }
+
+            if(pnl_fo.Controls.Count == 0)
+            {
+                MessageBox.Show("Gratulálok, megoldottál mindent!");
+            }
+            
+            
+
+
+        }
+
+        private void btn_bezaras_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
